8d79bdb7702bcdcc9e975fec2eeec4b5d5c2c570
Stepan <stepanpatricny@proton.me>
2026-09-02 15:19:03 +0200

    chore: mis en place du projet
    [50] [Done]

---
+++ PlotThoseLines/PlotThoseLines/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PlotThoseLines.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ PlotThoseLines/PlotThoseLines/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace PlotThoseLines.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
